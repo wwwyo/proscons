235ad57ce03791a3305598a8fbaf3553d2fd7979
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-11-05 15:51:52 +0900

    Add: factory bot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname { Faker::Lorem.word }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(min_length: 6) }
   end
 end
