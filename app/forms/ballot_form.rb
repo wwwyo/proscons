64b772a9e7ef4f9407daeaeaa6fe548bf3301cea
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-11-05 19:24:54 +0900

    Add: validation

--- app/forms/ballot_form.rb
+++ app/forms/ballot_form.rb
@@ -3,6 +3,11 @@ class BallotForm
   include ActiveModel::Model
   attr_accessor :question, :detail, :name, :user_id
 
+  with_options presence: true do
+    validates :question
+    validates :name
+  end
+
   def save
     ballot_box = BallotBox.create(question: question, detail: detail, user_id: user_id)
     tag = Tag.create(name: name)
