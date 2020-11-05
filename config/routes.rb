dc8a10d74bddf4b5df86ffd6d51d08c46521da92
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-11-05 10:58:24 +0900

    Add: user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'ballot_boxes/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: 'ballot_boxes#index'
 end
