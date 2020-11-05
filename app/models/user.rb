07689304a7b0d41c79052a805e3d27d788218cae
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-11-05 15:45:10 +0900

    Add: user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :nickname, presence: true
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
+  validates :password, format: { with: /\A[A-z0-9]+\z/ }
 end
