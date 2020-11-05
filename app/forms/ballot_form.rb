ceb2a4da16995ad1d2a98ba24548e4baf5799308
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-11-05 18:59:39 +0900

    Add: form object

---
+++ app/forms/ballot_form.rb
@@ -0,0 +1,11 @@
+class BallotForm
+
+  include ActiveModel::Model
+  attr_accessor :question, :detail, :name, :user_id
+
+  def save
+    ballot_box = BallotBox.create(question: question, detail: detail, user_id: user_id)
+    tag = Tag.create(name: name)
+    BallotTag.create(ballot_box_id: ballot_box.id, tag_id: tag.id)
+  end
+end
