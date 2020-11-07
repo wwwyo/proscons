a5b16c86eaf41bbdc77fb9197a705dab57a9ee4c
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-11-07 19:18:48 +0900

    Add: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'ballot_boxes#top'
-  resources :ballot_boxes
+  resources :ballot_boxes do
+    resources :votes, only: [:create]
+  end
 end
