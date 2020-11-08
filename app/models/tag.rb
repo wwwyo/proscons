f2512db7ee3273c85a2220fce2153dd02891138d
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-11-08 11:10:24 +0900

    Add: validation

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,5 +2,5 @@ class Tag < ApplicationRecord
   has_many :ballot_boxes, through: :ballot_tags
   has_many :ballot_tags, dependent: :destroy
 
-  validates :name, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 end
