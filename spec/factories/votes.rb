97ba4ce62cf8449e7deb55a56adc05badc36d475
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-11-08 15:25:53 +0900

    Add: test code (vote)

--- spec/factories/votes.rb
+++ spec/factories/votes.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :vote do
-    
+    result { "1" }
+    association :user
+    association :ballot_box
   end
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -6,10 +6,11 @@ RSpec.describe Vote, type: :model do
   end
   describe 'ballot_boxに対して賛成または反対を投票できる' do
     it 'resultに1が入っていると保存される' do
-
+      expect(@vote).to be_valid
     end
     it 'resultに0が入っていると保存される' do
-
+      @vote.result = "0"
+      expect(@vote).to be_valid
     end
   end
 end
