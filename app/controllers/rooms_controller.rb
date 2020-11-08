dc67c6049f94a68fdae41c92036fe95d49cbef46
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-11-08 20:20:36 +0900

    Improve: room controller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,13 @@
 class RoomsController < ApplicationController
+  before_action :room_name, only: [:index, :new, :show, :edit], if: :user_signed_in?
+
   def index
+    @room = BallotBox.find_by(id: params[:ballot_box_id])
+    @vote = Vote.find_by(user_id: current_user.id, ballot_box_id: params[:ballot_box_id])
+  end
+
+  private
+  def room_name
+    @rooms = current_user.user_rooms.includes(:room)
   end
 end
