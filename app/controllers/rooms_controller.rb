f06ccf45cc751f2fbfca203cf78009679c9ba12b
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-11-09 12:44:12 +0900

    Add: controller room destroy

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,9 +1,18 @@
 class RoomsController < ApplicationController
-  before_action :room_name, only: [:index, :new, :show, :edit], if: :user_signed_in?
+  before_action :room_name, only: [:index], if: :user_signed_in?
 
   def index
     @room = BallotBox.find(params[:ballot_box_id])
     @vote = Vote.find_by(user_id: current_user.id, ballot_box_id: params[:ballot_box_id])
+  end
+
+  def destroy
+    room = UserRoom.find_by(user_id: current_user.id, room_id: params[:id])
+    if room.destroy
+      redirect_to ballot_boxes_path
+    else
+      redirect_to action: :index
+    end
   end
 
   private
