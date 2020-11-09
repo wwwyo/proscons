e32e5c5294ad3423d74f839e7283f3effb87e49d
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-11-09 11:40:04 +0900

    Fix: params[:id]

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,7 +2,7 @@ class RoomsController < ApplicationController
   before_action :room_name, only: [:index, :new, :show, :edit], if: :user_signed_in?
 
   def index
-    @room = BallotBox.find_by(id: params[:ballot_box_id])
+    @room = BallotBox.find(params[:ballot_box_id])
     @vote = Vote.find_by(user_id: current_user.id, ballot_box_id: params[:ballot_box_id])
   end
 
