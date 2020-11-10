88dcfaf007f632738c78217ba37e4b54236e113e
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-11-10 13:37:18 +0900

    Improve: room view

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,10 +2,11 @@ class RoomsController < ApplicationController
   before_action :room_name, only: [:index], if: :user_signed_in?
 
   def index
-    @room = BallotBox.find(params[:ballot_box_id])
+    @ballot_room = BallotBox.find(params[:ballot_box_id])
     @vote = Vote.find_by(user_id: current_user.id, ballot_box_id: params[:ballot_box_id])
     @discussion = Discussion.new
-    @discussions = Discussion.all
+    @pros_votes = @ballot_room.votes.where(result: true)
+    @cons_votes = @ballot_room.votes.where(result: false)
   end
 
   def destroy
