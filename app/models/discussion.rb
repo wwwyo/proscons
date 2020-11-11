d73dec73925ab07126d32bd5955ba87651c790e4
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-11-11 12:48:35 +0900

    Add: association

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,6 +1,7 @@
 class Discussion < ApplicationRecord
   belongs_to :user
   belongs_to :room
+  has_many :likes, dependent: :destroy
   
   validates :comment, presence: true
   validates :vote_result, inclusion: {in: [true, false]}
