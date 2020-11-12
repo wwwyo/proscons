f1ddf0b9cebb3b0e3ed96c0a8b737df51c00f5d4
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-11-12 12:09:05 +0900

    Add: like.js
    asynchronous like function

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("../tag.js")
 require("../vote.js")
+require("../like.js")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
