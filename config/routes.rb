b48700c9ffbd8fb9aa970f70cfeb76ec96a2b54c
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-11-15 13:12:02 +0900

    Improve :side content
    Add: popular ballot_boxes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'ballot_boxes#top'
   resources :ballot_boxes do
+    collection do
+      get :popular
+    end
     resources :votes, only: [:create, :update]
     resources :rooms, only: [:index, :destroy] do
       resources :discussions, only: [:create] do
