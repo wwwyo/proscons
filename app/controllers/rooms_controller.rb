1cc852f3178eba12e55c5ed5f820b810db0e5bb0
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-11-15 16:58:59 +0900

    Add: view search header

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,6 @@
 class RoomsController < ApplicationController
   before_action :room_name, only: [:index], if: :user_signed_in?
-
+  before_action :search, only: [:index]
   def index
     @ballot_room = BallotBox.find(params[:ballot_box_id])
     @vote = Vote.find_by(user_id: current_user.id, ballot_box_id: params[:ballot_box_id])
@@ -20,5 +20,9 @@ class RoomsController < ApplicationController
   private
   def room_name
     @user_rooms = current_user.user_rooms.includes(:room)
+  end
+
+  def search
+    @search = Tag.ransack(params[:q])
   end
 end
