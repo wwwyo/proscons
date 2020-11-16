0ff1643f7270988c790072d2d92c60207e186f43
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-11-16 20:49:13 +0900

    Fix: #17

--- app/javascript/vote.js
+++ app/javascript/vote.js
@@ -1,13 +1,15 @@
-if (location.pathname.match("ballot_boxes/")){
+if (location.pathname.match("ballot_boxes/[0-9]\+\$")){
   document.addEventListener("DOMContentLoaded", () =>{
-    const prosBtn = document.getElementById("pros-btn");
-    const consBtn = document.getElementById("cons-btn");
     const voteForm = document.getElementById("vote-form");
-    prosBtn.addEventListener("click", () => {
-      voteForm.setAttribute("value", "1");
-    });
-    consBtn.addEventListener("click", () => {
-      voteForm.setAttribute("value", "0");
-    });
+    if ( voteForm != null ) {
+      const prosBtn = document.getElementById("pros-btn");
+      const consBtn = document.getElementById("cons-btn");
+      prosBtn.addEventListener("click", () => {
+        voteForm.setAttribute("value", "1");
+      });
+      consBtn.addEventListener("click", () => {
+        voteForm.setAttribute("value", "0");
+      });
+    }
   });
 }
