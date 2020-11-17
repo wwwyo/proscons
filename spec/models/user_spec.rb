13325bd99fb85e8de7a7c1c2c3f53ba174329140
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-11-17 18:58:54 +0900

    Change: user unit test
    English => japanese

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,17 +21,18 @@ RSpec.describe User, type: :model do
         expect(@user).to be_valid
       end
     end
+
     context '新規登録フォームを異常に記入すればアカウント登録できない' do 
       it 'nicknameが空欄' do
         @user.nickname = ""
         @user.valid?
-        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+        expect(@user.errors.full_messages).to include("ニックネームを入力してください")
       end
 
       it 'emailが空欄' do
         @user.email = ""
         @user.valid?
-        expect(@user.errors.full_messages).to include("Email can't be blank")
+        expect(@user.errors.full_messages).to include("メールアドレスを入力してください")
       end
 
       it 'emailが既に登録されているもの' do
@@ -39,31 +40,31 @@ RSpec.describe User, type: :model do
         user_test = FactoryBot.build(:user)
         user_test.email = @user.email
         user_test.valid?
-        expect(user_test.errors.full_messages).to include("Email has already been taken")
+        expect(user_test.errors.full_messages).to include("メールアドレスはすでに存在します")
       end
 
       it 'emailに@がない' do
         @user.email = "emailgmial.com"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Email is invalid")
+        expect(@user.errors.full_messages).to include("メールアドレスは不正な値です")
       end
 
       it 'passwordが空欄' do
         @user.password = ""
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password can't be blank")
+        expect(@user.errors.full_messages).to include("パスワードを入力してください")
       end
 
       it 'passwordが６文字未満' do
         @user.password = "12345"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+        expect(@user.errors.full_messages).to include("パスワードは6文字以上で入力してください")
       end
 
       it 'passwordを全角で入力' do
         @user.password = "１２３４５６"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is invalid")
+        expect(@user.errors.full_messages).to include("パスワードは不正な値です")
       end
     end
   end
