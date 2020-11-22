22189bedced5e5904f784e8ad20a82e251cdc9cc
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-11-22 13:39:37 +0900

    Improve: controller vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,20 +4,15 @@ class VotesController < ApplicationController
   def create
     vote = Vote.new(vote_params)
     if vote.save
-      UserRoom.create(user_id: current_user.id, room_id: vote.ballot_box.room.id)
+      UserRoom.create(user_id: current_user.id, room_id: vote.ballot_box.id)
       redirect_to ballot_box_rooms_path(params[:ballot_box_id])
     end
   end
 
   def update
     vote = Vote.find_by(user_id: current_user.id, ballot_box_id: params[:ballot_box_id])
-    if !vote.nil?
-      vote.result = if vote.result
-                      0
-                    else
-                      1
-                    end
-      vote.save
+    if vote.present?
+      Vote.change(vote)
       redirect_to ballot_box_rooms_path(params[:ballot_box_id])
     else
       redirect_to ballot_box_path(params[:ballot_box_id])
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,13 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :ballot_box
   validates :result, inclusion: { in: [true, false] }
+
+  def self.change(vote)
+    vote.result = if vote.result
+      0
+    else
+      1
+    end
+    vote.save
+  end
 end
