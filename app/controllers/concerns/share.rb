d742c529513f9528a3a6a777f2e7631c1390ce29
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-11-22 15:19:59 +0900

    Fix: N+1

--- app/controllers/concerns/share.rb
+++ app/controllers/concerns/share.rb
@@ -2,7 +2,7 @@ module Share
   extend ActiveSupport::Concern
 
   def share_content
-    @user_rooms = current_user.user_rooms.includes(:room).order(created_at: :desc) if user_signed_in?
+    @user_rooms = UserRoom.preload(room: :ballot_box).where(user_id: current_user.id).order(created_at: :desc) if user_signed_in?
     @search = Tag.ransack(params[:q])
   end
 end
