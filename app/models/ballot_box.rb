cde7740bf240c50943d1bbcceb90504b56d04252
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-11-26 14:51:12 +0900

    Fix: popular order

--- app/models/ballot_box.rb
+++ app/models/ballot_box.rb
@@ -4,4 +4,8 @@ class BallotBox < ApplicationRecord
   has_many :ballot_tags, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_one :room, dependent: :destroy
+
+  def self.search(search)
+    BallotBox.group('ballot_boxes.id').joins(:ballot_tags).where(ballot_tags: { tag_id: search.result.ids })
+  end
 end
